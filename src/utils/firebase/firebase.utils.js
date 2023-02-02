6e19d0b0a4a13e17f264c7a8bafafab345697f16
kim-ensu <kimdmitriiensu@gmail.com>
2023-02-02 22:04:38 +0600

    Add data to firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAQK7RQj_3_6ZHAQXdLPoacRgCmiwXj9B4",
@@ -34,6 +34,19 @@ export const signInWithGooglePopup = () => {
 };
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("done");
+};
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
   if (!userAuth) return;
