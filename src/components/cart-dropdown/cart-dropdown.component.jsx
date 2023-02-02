d3c21a647d5960c537c6d14c95df1be305caf398
kim-ensu <kimdmitriiensu@gmail.com>
2023-02-02 14:12:18 +0600

    Add add to cart logic for shop items

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,18 @@
 import "./cart-dropdown.styles.scss";
-import React from "react";
+import React, { useContext } from "react";
 import Button from "../button/button.components";
+import { CartContext } from "../../contexts/cart.context";
+import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
