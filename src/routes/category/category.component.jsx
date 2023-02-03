e8c8a37030db5acf039c2f09cb88e76c390f48f7
kim-ensu <kimdmitriiensu@gmail.com>
2023-02-03 16:42:10 +0600

    Add condition for products render at category component

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -7,7 +7,7 @@ import ProductCard from "../../components/product-card/product-card.component";
 const Category = () => {
   const { category } = useParams();
   const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
     setProducts(categoriesMap[category]);
@@ -15,9 +15,7 @@ const Category = () => {
 
   return (
     <div className="category-container">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+      {products && products.map((product) => <ProductCard key={product.id} product={product} />)}
     </div>
   );
 };
